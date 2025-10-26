4f4b72ecabc04300b69caf5c760c4cec970d59e0
John Marwin <84650390+Reposity23@users.noreply.github.com>
2025-10-27 01:29:34 +0800

    Update PROGRAM2.py

--- PROGRAM2.py
+++ PROGRAM2.py
@@ -1,12 +1,1 @@
-import tkinter as tk
-from tkinter import filedialog, scrolledtext, messagebox
-import threading
-import json
-import time
-from PIL import Image, ImageTk
-from xai_sdk import Client
-from xai_sdk.chat import system, user, assistant, image
-import os
-import base64
-import re
 
